d71c59e25cc1b76eb898992a71693e11a54b7f2f
Coursera Learner <nobody@nowhere.nul>
2020-07-13 09:31:57 +0000

    Did assignment 4

--- 05_squares/squares.c
+++ 05_squares/squares.c
@@ -46,6 +46,8 @@ void squares(int size1, int x_offset, int y_offset, int size2) {
     for(x = 0; x < w; x++){  
       //check if  EITHER
       if(((x >= x_offset && x < (x_offset + size2)) && (y == y_offset || y == (y_offset + size2 - 1))) || ((y >= y_offset && y < (y_offset + size2)) && (x == x_offset || x == (x_offset + size2 - 1))))
+	printf("*");
+      else if((x < size1 && (y == 0 || y == size1 - 1)) || (y < size1 && (x == 0 || x == size1 - 1)))
 	printf("#");
       else
 	printf(" ");
